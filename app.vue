3b0242182f555df3f0c907f7785092f85457a7f5
miruoon <rillakerika@gmail.com>
2021-10-17 11:55:13 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface ResData {
+  id: number;
+  title: string;
+}
+const { data: hello } = await useAsyncData(
+  "hello",
+  () => $fetch("/api/hello"),
+  { pick: ["hoge"] }
+);
+const { data: todo } = await useFetch<string, ResData>("/todos/1", {
+  pick: ["id", "title"],
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+</script>
+
+<template>
+  <p>{{ hello.hoge }}</p>
+  <p>{{ todo.id }} {{ todo.title }}</p>
+</template>
---
+++ server/api/hello.ts
@@ -0,0 +1,3 @@
+export default () => {
+  return { hello: "Hello World", hoge: "hoge" };
+};
